894529ced67d93bc3c1a3e0d3e2a859121432e07
Scarlett Wu <swu0207@berkeley.edu>
2019-12-11 13:43:48 -0800

    initial commit of basic react app

---
+++ build/precache-manifest.a8cd82f7867d9936a02bfedc5c23a2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a2315fa3e62f55db360fc9e53ffba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17fb4181e35a9f9cf374",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c413ca5e64f6ab13b045",
+    "url": "/static/js/2.7746f33b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7746f33b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17fb4181e35a9f9cf374",
+    "url": "/static/js/main.342500fa.chunk.js"
+  },
+  {
+    "revision": "faa7d32a05abe7611b02",
+    "url": "/static/js/runtime-main.bc632271.js"
+  }
+]);
